8986c94124624d47477746345ae4f89a34e2d180
Tim Benniks <tbenniks@gmail.com>
2017-07-15 23:04:03 +0200

    config vars

--- helpers/callApi.js
+++ helpers/callApi.js
@@ -1,3 +1,3 @@
 const request = require('request-promise-native')
 
-module.exports = (url) => request({ uri: url, json: true, headers: { 'Authorization': 'Token token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhcHBfdXVpZCI6IjcyOGQ3YTkzLTMwNWQtNGNkOS05NDQxLTAwZDY5NTczODc4MiIsImlkIjo1NiwidXBkYXRlZF9hdCI6IjIwMTctMDctMTUgMTM6NDA6MTIgVVRDIn0.9t0JVc_J7VrHcSSmtjUuw7oW7LRo_Q-QuLoy8H7mf_A' }})
+module.exports = (url) => request({ uri: url, json: true, headers: { 'Authorization': `Token token=${process.env.ELEGANT_CMS_TOKEN}` }})
